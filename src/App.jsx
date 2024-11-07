614de90daedb283af48846033f56cd4d408c9b00
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-11-08 00:50:57 +0500

    create card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,175 @@
-
 function App() {
-
   return (
     <>
-      REACT FIREBASE
+      <section className="bg-sky-100">
+        <button className="w-[200px] px-1 py-1 m-4 bg-sky-500 text-white hover:text-sky-500 hover:bg-white font-semibold transition-all duration-200 rounded-lg">
+          <i class="bi bi-node-plus-fill"></i> &nbsp; Add User
+        </button>
+      </section>
+
+      <section className="p-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+          <div className="m-2 grid grid-cols-4 border-sky-200 border p-3 rounded-lg text-sky-500">
+            <img
+              src="https://picsum.photos/300/400"
+              alt=""
+              className="w-[100px] h-[100px] object-cover rounded-full"
+            />
+
+            <div className="col-span-3">
+              <p>
+                <span className="font-bold">First name: </span>{" "}
+                <span>Shahboz</span>
+              </p>
+              <p>
+                <span className="font-bold">Last name: </span>{" "}
+                <span>Shirinboyev</span>
+              </p>
+              <p>
+                <span className="font-bold">Birth date: </span>{" "}
+                <span>12.03.1999</span>
+              </p>
+              <p>
+                <span className="font-bold">Phone number: </span>{" "}
+                <span>+998 93 009 11 66</span>
+              </p>
+            </div>
+          </div>
+          <div className="m-2 grid grid-cols-4 border-sky-200 border p-3 rounded-lg text-sky-500">
+            <img
+              src="https://picsum.photos/300/400"
+              alt=""
+              className="w-[100px] h-[100px] object-cover rounded-full"
+            />
+
+            <div className="col-span-3">
+              <p>
+                <span className="font-bold">First name: </span>{" "}
+                <span>Shahboz</span>
+              </p>
+              <p>
+                <span className="font-bold">Last name: </span>{" "}
+                <span>Shirinboyev</span>
+              </p>
+              <p>
+                <span className="font-bold">Birth date: </span>{" "}
+                <span>12.03.1999</span>
+              </p>
+              <p>
+                <span className="font-bold">Phone number: </span>{" "}
+                <span>+998 93 009 11 66</span>
+              </p>
+            </div>
+          </div>
+          <div className="m-2 grid grid-cols-4 border-sky-200 border p-3 rounded-lg text-sky-500">
+            <img
+              src="https://picsum.photos/300/400"
+              alt=""
+              className="w-[100px] h-[100px] object-cover rounded-full"
+            />
+
+            <div className="col-span-3">
+              <p>
+                <span className="font-bold">First name: </span>{" "}
+                <span>Shahboz</span>
+              </p>
+              <p>
+                <span className="font-bold">Last name: </span>{" "}
+                <span>Shirinboyev</span>
+              </p>
+              <p>
+                <span className="font-bold">Birth date: </span>{" "}
+                <span>12.03.1999</span>
+              </p>
+              <p>
+                <span className="font-bold">Phone number: </span>{" "}
+                <span>+998 93 009 11 66</span>
+              </p>
+            </div>
+          </div>
+          <div className="m-2 grid grid-cols-4 border-sky-200 border p-3 rounded-lg text-sky-500">
+            <img
+              src="https://picsum.photos/300/400"
+              alt=""
+              className="w-[100px] h-[100px] object-cover rounded-full"
+            />
+
+            <div className="col-span-3">
+              <p>
+                <span className="font-bold">First name: </span>{" "}
+                <span>Shahboz</span>
+              </p>
+              <p>
+                <span className="font-bold">Last name: </span>{" "}
+                <span>Shirinboyev</span>
+              </p>
+              <p>
+                <span className="font-bold">Birth date: </span>{" "}
+                <span>12.03.1999</span>
+              </p>
+              <p>
+                <span className="font-bold">Phone number: </span>{" "}
+                <span>+998 93 009 11 66</span>
+              </p>
+            </div>
+          </div>
+          <div className="m-2 grid grid-cols-4 border-sky-200 border p-3 rounded-lg text-sky-500">
+            <img
+              src="https://picsum.photos/300/400"
+              alt=""
+              className="w-[100px] h-[100px] object-cover rounded-full"
+            />
+
+            <div className="col-span-3">
+              <p>
+                <span className="font-bold">First name: </span>{" "}
+                <span>Shahboz</span>
+              </p>
+              <p>
+                <span className="font-bold">Last name: </span>{" "}
+                <span>Shirinboyev</span>
+              </p>
+              <p>
+                <span className="font-bold">Birth date: </span>{" "}
+                <span>12.03.1999</span>
+              </p>
+              <p>
+                <span className="font-bold">Phone number: </span>{" "}
+                <span>+998 93 009 11 66</span>
+              </p>
+            </div>
+          </div>
+          <div className="m-2 grid grid-cols-4 border-sky-200 border p-3 rounded-lg text-sky-500">
+            <img
+              src="https://picsum.photos/300/400"
+              alt=""
+              className="w-[100px] h-[100px] object-cover rounded-full"
+            />
+
+            <div className="col-span-3">
+              <p>
+                <span className="font-bold">First name: </span>{" "}
+                <span>Shahboz</span>
+              </p>
+              <p>
+                <span className="font-bold">Last name: </span>{" "}
+                <span>Shirinboyev</span>
+              </p>
+              <p>
+                <span className="font-bold">Birth date: </span>{" "}
+                <span>12.03.1999</span>
+              </p>
+              <p>
+                <span className="font-bold">Phone number: </span>{" "}
+                <span>+998 93 009 11 66</span>
+              </p>
+            </div>
+          </div>
+          
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
